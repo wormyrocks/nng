0916961298f28d7c62deecfde8557b302ffbb858
evan kahn <ekahn@hmc.edu>
2019-06-25 12:55:02 -0400

    added mbedtls, patched win_thread

---
+++ build.sh
@@ -0,0 +1,7 @@
+source ~/env_setup.sh
+mkdir -p build
+rm -rf build/*
+cd build
+cmake -G "MinGW Makefiles" .. -DNNG_STATIC_LIB=ON -DBUILD_SHARED_LIBS=OFF -DNNG_ENABLE_TLS=ON -DNNG_TOOLS=OFF -DNNG_TESTS=ON -DMBEDTLS_ROOT_DIR=../mbedtls -DMBEDTLS_INCLUDE_DIR=../mbedtls -DMBEDTLS_CRYPTO_LIBRARY=../mbedtls/libmbedcrypto.a -DMBEDTLS_TLS_LIBRARY=../mbedtls/libmbedtls.a -DMBEDTLS_X509_LIBRARY=../mbedtls/libmbedx509.a
+cmake ..
+cmake --build .
