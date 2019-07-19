16b678e89e679a50ebee58f959594570dff7e542
evan kahn <ekahn@hmc.edu>
2019-07-19 12:04:25 -0400

    builds nngcat

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 source ~/env_setup.sh
 mkdir -p build
 cd build
-cmake -G "MinGW Makefiles" .. -DNNG_STATIC_LIB=ON -DBUILD_SHARED_LIBS=OFF -DNNG_ENABLE_TLS=ON -DNNG_TOOLS=OFF -DNNG_TESTS=ON -DMBEDTLS_ROOT_DIR=../mbedtls -DMBEDTLS_INCLUDE_DIR=../mbedtls -DMBEDTLS_CRYPTO_LIBRARY=../mbedtls/libmbedcrypto.a -DMBEDTLS_TLS_LIBRARY=../mbedtls/libmbedtls.a -DMBEDTLS_X509_LIBRARY=../mbedtls/libmbedx509.a
+cmake -G "MinGW Makefiles" .. -DNNG_STATIC_LIB=ON -DBUILD_SHARED_LIBS=OFF -DNNG_ENABLE_TLS=ON -DNNG_TOOLS=OFF -DNNG_TESTS=OFF -DMBEDTLS_ROOT_DIR=../mbedtls -DMBEDTLS_INCLUDE_DIR=../mbedtls -DMBEDTLS_CRYPTO_LIBRARY=../mbedtls/libmbedcrypto.a -DMBEDTLS_TLS_LIBRARY=../mbedtls/libmbedtls.a -DMBEDTLS_X509_LIBRARY=../mbedtls/libmbedx509.a -DNNG_ENABLE_NNGCAT=ON
 cmake ..
-mingw32-make nng
+mingw32-make nng nngcat
