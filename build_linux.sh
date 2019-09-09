050c39ee1f9c5dde3a6a4b5008e21d84b6ae1c37
evan kahn <ekahn@hmc.edu>
2019-09-09 12:05:08 -0400

    Stripped out mbedtls library

--- build_linux.sh
+++ build_linux.sh
@@ -1,5 +1,5 @@
 mkdir -p build
 cd build
-cmake .. -DNNG_STATIC_LIB=ON -DBUILD_SHARED_LIBS=OFF -DNNG_ENABLE_TLS=ON -DNNG_TOOLS=OFF -DNNG_TESTS=OFF -DMBEDTLS_ROOT_DIR=../mbedtls -DMBEDTLS_INCLUDE_DIR=../mbedtls -DMBEDTLS_CRYPTO_LIBRARY=../mbedtls/linux/libmbedcrypto.a -DMBEDTLS_TLS_LIBRARY=../mbedtls/linux/libmbedtls.a -DMBEDTLS_X509_LIBRARY=../mbedtls/linux/libmbedx509.a -DNNG_ENABLE_NNGCAT=ON
+cmake .. -DNNG_STATIC_LIB=ON -DBUILD_SHARED_LIBS=OFF -DNNG_TOOLS=OFF -DNNG_TESTS=OFF -DNNG_ENABLE_NNGCAT=ON
 cmake ..
 make nng nngcat
